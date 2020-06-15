b7570ab74ad0bd2f49dfdddb6837ac11b2a898f7
Kautenja <ckfour@icloud.com>
2020-06-15 05:06:16 -0500

    remove main_bus cpp file

--- src/nes/main_bus.hpp
+++ src/nes/main_bus.hpp
@@ -13,6 +13,7 @@
 #include <unordered_map>
 #include "common.hpp"
 #include "mapper.hpp"
+#include "log.hpp"
 
 namespace NES {
 
@@ -80,32 +81,15 @@ class MainBus {
     /// Initialize a new main bus.
     MainBus() : ram(0x800, 0), mapper(nullptr) { }
 
-    /// Return a 8-bit pointer to the RAM buffer's first address.
-    ///
-    /// @return a 8-bit pointer to the RAM buffer's first address
-    ///
-    inline NES_Byte* get_memory_buffer() { return &ram.front(); }
-
-    /// Read a byte from an address on the RAM.
-    ///
-    /// @param address the 16-bit address of the byte to read in the RAM
-    ///
-    /// @return the byte located at the given address
-    ///
-    NES_Byte read(NES_Address address);
-
-    /// Write a byte to an address in the RAM.
-    ///
-    /// @param address the 16-bit address to write the byte to in RAM
-    /// @param value the byte to write to the given address
-    ///
-    void write(NES_Address address, NES_Byte value);
-
     /// Set the mapper pointer to a new value.
     ///
     /// @param mapper the new mapper pointer for the bus to use
     ///
-    void set_mapper(Mapper* mapper);
+    void set_mapper(Mapper* mapper) {
+        this->mapper = mapper;
+        if (mapper->hasExtendedRAM())
+            extended_ram.resize(0x2000);
+    }
 
     /// Set a callback for when writes occur.
     inline void set_write_callback(IORegisters reg, WriteCallback callback) {
@@ -118,7 +102,97 @@ class MainBus {
     }
 
     /// Return a pointer to the page in memory.
-    const NES_Byte* get_page_pointer(NES_Byte page);
+    const NES_Byte* get_page_pointer(NES_Byte page) {
+        NES_Address address = page << 8;
+        if (address < 0x2000)
+            return &ram[address & 0x7ff];
+        else if (address < 0x4020)
+            LOG(Error) << "Register address memory pointer access attempt" << std::endl;
+        else if (address < 0x6000)
+            LOG(Error) << "Expansion ROM access attempted, which is unsupported" << std::endl;
+        else if (address < 0x8000)
+            if (mapper->hasExtendedRAM())
+                return &extended_ram[address - 0x6000];
+
+        return nullptr;
+    }
+
+    /// Return a 8-bit pointer to the RAM buffer's first address.
+    ///
+    /// @return a 8-bit pointer to the RAM buffer's first address
+    ///
+    inline NES_Byte* get_memory_buffer() { return &ram.front(); }
+
+    /// Read a byte from an address on the RAM.
+    ///
+    /// @param address the 16-bit address of the byte to read in the RAM
+    ///
+    /// @return the byte located at the given address
+    ///
+    NES_Byte read(NES_Address address) {
+        if (address < 0x2000) {
+            return ram[address & 0x7ff];
+        } else if (address < 0x4020) {
+            if (address < 0x4000) {  // PPU registers, mirrored
+                auto reg = static_cast<IORegisters>(address & 0x2007);
+                if (read_callbacks.count(reg))
+                    return read_callbacks.at(reg)();
+                else
+                    LOG(InfoVerbose) << "No read callback registered for I/O register at: " << std::hex << +address << std::endl;
+            } else if (address < 0x4018 && address >= 0x4000) {  // only *some* IO registers (mostly APU)
+                auto reg = static_cast<IORegisters>(address);
+                if (read_callbacks.count(reg))
+                    return read_callbacks.at(reg)();
+                else
+                    LOG(InfoVerbose) << "No read callback registered for I/O register at: " << std::hex << +address << std::endl;
+            }
+            else {
+                LOG(InfoVerbose) << "Read access attempt at: " << std::hex << +address << std::endl;
+            }
+        } else if (address < 0x6000) {
+            LOG(InfoVerbose) << "Expansion ROM read attempted. This is currently unsupported" << std::endl;
+        } else if (address < 0x8000) {
+            if (mapper->hasExtendedRAM())
+                return extended_ram[address - 0x6000];
+        } else {
+            return mapper->readPRG(address);
+        }
+        return 0;
+    }
+
+    /// Write a byte to an address in the RAM.
+    ///
+    /// @param address the 16-bit address to write the byte to in RAM
+    /// @param value the byte to write to the given address
+    ///
+    void write(NES_Address address, NES_Byte value) {
+        if (address < 0x2000) {
+            ram[address & 0x7ff] = value;
+        } else if (address < 0x4020) {
+            if (address < 0x4000) {  // PPU registers, mirrored
+                auto reg = static_cast<IORegisters>(address & 0x2007);
+                if (write_callbacks.count(reg))
+                    return write_callbacks.at(reg)(value);
+                else
+                    LOG(InfoVerbose) << "No write callback registered for I/O register at: " << std::hex << +address << std::endl;
+            } else if (address < 0x4018 && address >= 0x4000) {  // only some registers (mostly APU)
+                auto reg = static_cast<IORegisters>(address);
+                if (write_callbacks.count(reg))
+                    return write_callbacks.at(reg)(value);
+                else
+                    LOG(InfoVerbose) << "No write callback registered for I/O register at: " << std::hex << +address << std::endl;
+            } else {
+                LOG(InfoVerbose) << "Write access attmept at: " << std::hex << +address << std::endl;
+            }
+        } else if (address < 0x6000) {
+            LOG(InfoVerbose) << "Expansion ROM access attempted. This is currently unsupported" << std::endl;
+        } else if (address < 0x8000) {
+            if (mapper->hasExtendedRAM())
+                extended_ram[address - 0x6000] = value;
+        } else {
+            mapper->writePRG(address, value);
+        }
+    }
 };
 
 }  // namespace NES
