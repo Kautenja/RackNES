17243371b9c5e40e1295267261dc613e7c0a14e6
Kautenja <ckfour@icloud.com>
2020-06-17 01:01:16 -0500

    typedef for callback

--- src/nes/common.hpp
+++ src/nes/common.hpp
@@ -11,6 +11,7 @@
 // resolve an issue with MSVC overflow during compilation (Windows)
 #define _CRT_DECLARE_NONSTDC_NAMES 0
 #include <cstdint>
+#include <functional>
 
 namespace NES {
 
@@ -20,6 +21,8 @@ typedef uint8_t NES_Byte;
 typedef uint16_t NES_Address;
 /// A shortcut for a single pixel in memory
 typedef uint32_t NES_Pixel;
+/// a type definition for a basic callback method
+typedef std::function<void(void)> Callback;
 
 }  // namespace NES
 
