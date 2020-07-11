ad6745c3bf2104e59ad67ee9289c2f7210045ea1
Kautenja <kautencreations@gmail.com>
2020-07-10 22:11:10 -0500

    use decimal instead of hex for readability

--- src/nes/cpu_opcodes.hpp
+++ src/nes/cpu_opcodes.hpp
@@ -248,7 +248,7 @@ enum class OpcodeTable: NES_Byte {
 };
 
 /// a mapping of opcodes to the number of cycles used by the opcode.
-const NES_Byte OPERATION_CYCLES[0x100] = {
+const NES_Byte OPERATION_CYCLES[256] = {
 //  0 1 2 3 4 5 6 7 8 9 A B C D E F
     0,6,2,8,3,3,5,5,3,2,2,2,4,4,6,6,// 0
     3,5,2,8,4,4,6,6,2,4,2,7,4,4,7,7,// 1
